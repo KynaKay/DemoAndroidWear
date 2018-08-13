a23d648538b04a22e9d8548e3e828338c634a3d3
RAJA NUR SAKINAH BTE RAJA M A <16046508@rp.edu.sg>
2018-08-13 15:53:19 +0800

    Initial commit

---
+++ mobile/src/main/java/sg/edu/rp/demoandroidwear/ReplyActivity.java
@@ -0,0 +1,29 @@
+package sg.edu.rp.demoandroidwear;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.RemoteInput;
+import android.widget.Toast;
+
+public class ReplyActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reply);
+
+        CharSequence reply = null;
+        Intent intent = getIntent();
+        Bundle remoteInput = RemoteInput.getResultsFromIntent(intent);
+        if (remoteInput != null){
+            reply = remoteInput.getCharSequence("status");
+        }
+
+        if(reply != null){
+            Toast.makeText(ReplyActivity.this, "You have indicated: " + reply,
+                    Toast.LENGTH_SHORT).show();
+        }
+
+    }
+}
